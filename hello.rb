7d48228ed8ae12f6423a74f3b58a661e5a82b7d1
Sherfin Shamsudeen <sherfin94@gmail.com>
2017-07-30 18:21:03 +0530

    Update chit information

--- hello.rb
+++ hello.rb
@@ -9,41 +9,81 @@ get '/' do
         id: '1',
         name: 'KSFE Chit fund',
         location: 'Aluva',
-        amount: '100000',
-        installments: '10',
-        months: 18
+        amount: '2000000',
+        installments: '40',
+        months: 40
       },
       {
         id: '2',
         name: 'KSFE Chit fund',
         location: 'Aluva',
         amount: '1000000',
-        installments: '10',
-        months: 18
+        installments: '40',
+        months: 40
       },
       {
         id: '3',
         name: 'KSFE Chit fund',
         location: 'Aluva',
-        amount: '7000000',
-        installments: '10',
-        months: 18
+        amount: '1000000',
+        installments: '100',
+        months: 100
       },
       {
         id: '4',
         name: 'KSFE Chit fund',
         location: 'Aluva',
-        amount: '4000000',
-        installments: '10',
-        months: 18
+        amount: '500000',
+        installments: '50',
+        months: 50
       },
       {
         id: '5',
         name: 'KSFE Chit fund',
         location: 'Aluva',
         amount: '5000000',
-        installments: '10',
-        months: 18
+        installments: '100',
+        months: 100
+      },
+      {
+        id: '6',
+        name: 'KSFE Chit fund',
+        location: 'Aluva',
+        amount: '30000',
+        installments: '40',
+        months: 40
+      },
+      {
+        id: '7',
+        name: 'KSFE Chit fund',
+        location: 'Aluva',
+        amount: '200000',
+        installments: '30',
+        months: 30
+      },
+      {
+        id: '8',
+        name: 'KSFE Chit fund',
+        location: 'Aluva',
+        amount: '120000',
+        installments: '30',
+        months: 30
+      },
+      {
+        id: '9',
+        name: 'KSFE Chit fund',
+        location: 'Aluva',
+        amount: '100000',
+        installments: '40',
+        months: 40
+      },
+      {
+        id: '10',
+        name: 'KSFE Chit fund',
+        location: 'Aluva',
+        amount: '50000',
+        installments: '50',
+        months: 50
       }
     ]
   }
